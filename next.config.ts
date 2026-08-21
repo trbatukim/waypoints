415db0d9b08498d5de2ad4ff9d335a9ca68efab4
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-08-21 12:19:41 +0300

    fixed supabase login not working

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import dns from 'node:dns'
+import { setDefaultAutoSelectFamilyAttemptTimeout } from 'node:net'
+import { setGlobalDispatcher, Agent } from 'undici'
+
+setGlobalDispatcher(new Agent({ connect: { family: 4 } }))
+setDefaultAutoSelectFamilyAttemptTimeout(100)
+dns.setDefaultResultOrder('ipv4first')
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Needed for JS to work on mobile over LAN
+  allowedDevOrigins: ['192.168.1.178', '192.168.0.192'],
 };
 
 export default nextConfig;
