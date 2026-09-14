bdcc8a44e3f532759cdce248f497ede636c8b7e2
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-09-14 15:20:51 +0200

    stuck zoom fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export const viewport: Viewport = {
     width: "device-width",
     initialScale: 1,
     viewportFit: "cover",
+    maximumScale: 1,
     themeColor: [
         { media: "(prefers-color-scheme: light)", color: "#ffffff" },
         { media: "(prefers-color-scheme: dark)", color: "#0a0a0a" },
